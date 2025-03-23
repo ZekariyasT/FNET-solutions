3c204853748e564f8b16fecdc7cb81f3de033223
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-23 14:34:43 +0300

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-purple-400/20 to-amber-200/20 rounded-full  blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
   );
 }
 
